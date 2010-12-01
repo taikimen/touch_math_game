35085e7c1162080f0c8c6e169404b9b7d11401c3
Taiki Shimazu <taiki@elem.eco.to>
2010-12-01 22:56:38 +0900

    first commit

---
+++ src/net/nha_tien/touch_math_game/controller/IndexController.java
@@ -0,0 +1,12 @@
+package net.nha_tien.touch_math_game.controller;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+public class IndexController extends Controller {
+
+    @Override
+    public Navigation run() throws Exception {
+        return forward("index.jsp");
+    }
+}
